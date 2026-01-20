2c4cdfd77f68bac72d55af96c2a898958355f80b
Aiden van Wijnbergen <s1138387@student.hsleiden.nl>
2026-01-20 10:44:31 +0100

    feat: Added standard errors

--- internal/utils/errors.go
+++ internal/utils/errors.go
@@ -1,3 +1,16 @@
 package utils
 
+import "errors"
+
 // Responsible for handling errors
+
+var (
+	// Meshy lifecycle errors
+	ErrMeshyJobFailed = errors.New("meshy job failed")
+	ErrMeshyTimeout   = errors.New("meshy job timed out")
+	ErrMeshyInvalid   = errors.New("invalid meshy response")
+
+	// Job runner errors
+	ErrJobCancelled    = errors.New("job cancelled")
+	ErrInvalidJobState = errors.New("invalid job state")
+)
